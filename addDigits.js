ae91c2352c8240efd637a3b83ee79c001ff7846f
Ashwini <shahi.ashwini29@gmail.com>
2023-05-26 08:15:11 +0530

    Problem - 258. Add Digits

---
+++ addDigits.js
@@ -0,0 +1,44 @@
+/**
+ ************************************************************
+                     258. Add Digits
+ ************************************************************
+
+Given an integer num, repeatedly add all its digits until the result has only one digit, and return it.
+
+ 
+
+Example 1:
+
+Input: num = 38
+Output: 2
+Explanation: The process is
+38 --> 3 + 8 --> 11
+11 --> 1 + 1 --> 2 
+Since 2 has only one digit, return it.
+Example 2:
+
+Input: num = 0
+Output: 0
+ 
+
+Constraints:
+
+0 <= num <= 231 - 1
+ 
+
+Follow up: Could you do it without any loop/recursion in O(1) runtime?
+ */
+let num = 12345;
+let current = num;
+while (current > 9) {
+    let len = current.toString().length;
+    console.log(`len = ${len}`);
+    let sum = 0;
+    for (i = 0; i < len; i++) {
+        let c = current.toString().charAt(i);
+        sum += parseInt(c);
+    }
+    current = sum;
+    console.log(`current = ${current}`);
+}
+console.log(current);
